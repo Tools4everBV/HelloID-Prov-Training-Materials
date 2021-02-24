f559f7f04c896d74ee8a5aa8070099524b8012bb
Grady Koopman <g.koopman@tools4ever.com>
2021-02-24 11:25:14 +0100

    Initial commit

---
+++ lab files/lab 1/departments.ps1
@@ -0,0 +1,28 @@
+$importSourcePath = "C:\Users\s.vandijk\Downloads\TEMP\DemoData\"
+$delimiter = ";"
+
+function Get-SourceConnectorData { 
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory = $true)]$SourceFile,
+        [parameter(Mandatory = $true)][ref]$data
+    )
+      
+    try {
+        $dataset = Import-Csv -Path "$importSourcePath$SourceFile" -Delimiter $delimiter
+
+        foreach ($record in $dataset) { 
+            $data.Value.add($record) 
+        }
+    }
+    catch {
+        $data.Value = $null
+        Write-Verbose $_.Exception
+    }
+}
+
+$organizationalUnits = New-Object System.Collections.ArrayList
+Get-SourceConnectorData -SourceFile "T4E_HelloID_OrganizationalUnits.csv" ([ref]$organizationalUnits)
+
+$json = $organizationalUnits | ConvertTo-Json -Depth 3
+Write-Output $json
---
+++ lab files/lab 4/create.ps1
@@ -0,0 +1,51 @@
+
+#Initialize default properties
+$p = $person | ConvertFrom-Json;
+$success = $False;
+$auditMessage = "for person " + $p.DisplayName;
+
+$account_guid = New-Guid
+
+#Build you generation of the DisplayName
+<logic>
+
+#Change mapping here
+$account = [PSCustomObject]@{
+    displayName = $p.DisplayName;
+    firstName= $p.Name.NickName;
+    lastName= $p.Name.FamilyName;
+    userName = $p.UserName;
+    externalId = $account_guid;
+    title = $p.PrimaryContract.Title.Name;
+    department = $p.PrimaryContract.Department.DisplayName;
+    startDate = $p.PrimaryContract.StartDate;
+    endDate = $p.PrimaryContract.EndDate;
+    manager = $p.PrimaryManager.DisplayName;
+}
+
+$path = "<Use connector configuration>"
+$file = $path + "<Use connector configuration>"
+$Delimiter = ";"
+
+Try{
+    if(-Not($dryRun -eq $True)) {
+    $account | Export-Csv $file -Delimiter $Delimiter -NoTypeInformation -Append
+}
+    $success = $True;
+    $auditMessage = "export succesfully";
+}
+
+Catch{
+    $auditMessage = "export failes"
+}
+
+#build up result
+$result = [PSCustomObject]@{ 
+	Success= $success;
+	AccountReference= $account_guid;
+	AuditDetails=$auditMessage;
+    Account = $account;
+};
+
+#send result back
+Write-Output $result | ConvertTo-Json -Depth 10
