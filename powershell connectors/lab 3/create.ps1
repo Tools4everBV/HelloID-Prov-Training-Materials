059d4446838f3b3df9cdbb29bf3b9b22402d20f1
r.jongbloed@tools4ever.com <r.jongbloed@tools4ever.com>
2025-03-13 20:56:07 +0100

    first change

--- powershell connectors/lab 3/create.ps1
+++ powershell connectors/lab 3/create.ps1
@@ -1,88 +1,164 @@
-#Initialize default properties
-$path = "<Use connector configuration from UI component>"
-$path = $path -replace '[\\/]?[\\/]$'
-$file = "<Use connector configuration from UI component>"
-$exportPath = $path + "\" + $file
-$delimiter = ";"
+#################################################
+# HelloID-Conn-Prov-Target-TrainingIntranet-Create
+# PowerShell V2
+#################################################
 
-# Check if we should try to correlate the account
-if ($actionContext.CorrelationConfiguration.Enabled) {
-    $correlationField = $actionContext.CorrelationConfiguration.accountField
-    $correlationValue = $actionContext.CorrelationConfiguration.accountFieldValue
+# Enable TLS1.2
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-    if ($correlationField -eq $null) {
-        Write-Warning "Correlation is enabled but not configured correctly."
-    }
-
-    #  Write logic here that checks if the account can be correlated in the target system
-
-    if ($correlatedAccount -ne $null) {
-        $outputContext.AccountReference = $correlatedAccount.ExternalId
-        $outputContext.Data.ExternalId = $correlatedAccount.ExternalId
-
-        $outputContext.AuditLogs.Add([PSCustomObject]@{
-                Action  = "CorrelateAccount" # Optionally specify a different action for this audit log
-                Message = "Correlated account with username $($correlatedAccount.UserName) on field $($correlationField) with value $($correlationValue)"
-                IsError = $false
-            })
-
-        $outputContext.Success = $True
-        $outputContext.AccountCorrelated = $True
+#region functions
+function Resolve-TrainingIntranetError {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [object]
+        $ErrorObject
+    )
+    process {
+        $httpErrorObj = [PSCustomObject]@{
+            ScriptLineNumber = $ErrorObject.InvocationInfo.ScriptLineNumber
+            Line             = $ErrorObject.InvocationInfo.Line
+            ErrorDetails     = $ErrorObject.Exception.Message
+            FriendlyMessage  = $ErrorObject.Exception.Message
+        }
+        if (-not [string]::IsNullOrEmpty($ErrorObject.ErrorDetails.Message)) {
+            $httpErrorObj.ErrorDetails = $ErrorObject.ErrorDetails.Message
+        } elseif ($ErrorObject.Exception.GetType().FullName -eq 'System.Net.WebException') {
+            if ($null -ne $ErrorObject.Exception.Response) {
+                $streamReaderResponse = [System.IO.StreamReader]::new($ErrorObject.Exception.Response.GetResponseStream()).ReadToEnd()
+                if (-not [string]::IsNullOrEmpty($streamReaderResponse)) {
+                    $httpErrorObj.ErrorDetails = $streamReaderResponse
+                }
+            }
+        }
+        try {
+            $errorDetailsObject = ($httpErrorObj.ErrorDetails | ConvertFrom-Json)
+            # Make sure to inspect the error result object and add only the error message as a FriendlyMessage.
+            # $httpErrorObj.FriendlyMessage = $errorDetailsObject.message
+            $httpErrorObj.FriendlyMessage = $httpErrorObj.ErrorDetails # Temporarily assignment
+        } catch {
+            $httpErrorObj.FriendlyMessage = $httpErrorObj.ErrorDetails
+        }
+        Write-Output $httpErrorObj
     }
 }
+#endregion
 
-if (!$outputContext.AccountCorrelated) {
-    # Create account object from mapped data and set the correct account reference
-    $account = $actionContext.Data
-    $outputContext.AccountReference = $account.ExternalId
+#region training functions
+function Get-CsvUser {
+    param (
+        [string]
+        $Path,
 
-    $incompleteAccount = $false
+        [string]
+        $Delimiter,
 
-    if ($account.firstName -eq $null) {
-        $incompleteAccount = $true
-        Write-Warning "Person does not have a firstname"
+        [string]
+        $CorrelationField,
+
+        [string]
+        $correlationValue
+    )
+    $data = Import-Csv -Path $Path -Delimiter $delimiter
+    $user = $data | Where-Object { $_.$correlationField -eq $correlationValue }
+    Write-Output $user
+}
+#endregion
+
+try {
+    # Initial Assignments
+    $outputContext.AccountReference = 'Currently not available'
+
+    # Validate correlation configuration
+    if ($actionContext.CorrelationConfiguration.Enabled) {
+        $correlationField = $actionContext.CorrelationConfiguration.AccountField
+        $correlationValue = $actionContext.CorrelationConfiguration.PersonFieldValue
+
+        if ([string]::IsNullOrEmpty($($correlationField))) {
+            throw 'Correlation is enabled but not configured correctly'
+        }
+        if ([string]::IsNullOrEmpty($($correlationValue))) {
+            throw 'Correlation is enabled but [accountFieldValue] is empty. Please make sure it is correctly mapped'
+        }
+
+        # Determine if a user needs to be [created] or [correlated]
+
+        # Example of replacing the placeholder with actual code:
+        # Retrieve user details using an API call and store the result in $correlatedAccount
+        # $correlatedAccount = Invoke-RestMethod @splatGetUserParams
+
+        # Start van antwoord van Lab 7.1 correlatie in create script maken:
+        $splatGetCsvUserParams = @{
+            Path             = "C:\Users\ricad\OneDrive - Tools4ever B.V\Documenten\Tools4everGit\HelloID-Prov-Training-Materials\powershell connectors\lab 3\storage.csv"  # Moet configuratieoptie worden
+            Delimiter        = ";"  # Moet configuratieoptie worden
+            CorrelationField = $correlationField
+            CorrelationValue = $correlationValue
+        }
+        $correlatedAccount = Get-CsvUser @splatGetCsvUserParams
+        # Eind van antwoord van Lab 7.1 correlatie in create script maken:
     }
 
-    if ($incompleteAccount) {
-        $outputContext.Success = $false
-
-        $outputContext.AuditLogs.Add([PSCustomObject]@{
-                Action  = "CreateAccount" # Optionally specify a different action for this audit log
-                Message = "Creating account failed: Person does not have a firstname"
-                IsError = $True
-            })
+    if ($null -ne $correlatedAccount) {
+        $action = 'CorrelateAccount'
+    } else {
+        $action = 'CreateAccount'
     }
-    else {
-        Try {
-            if (-Not($actionContext.DryRun -eq $true)) {
-                #please enter your data export logic here
 
-                $outputContext.Success = $true
-
-                $outputContext.AuditLogs.Add([PSCustomObject]@{
-                        Action  = "CreateAccount" # Optionally specify a different action for this audit log
-                        Message = "Export succesfully with username $($account.UserName)"
-                        IsError = $false
-                    })
+    # Process
+    switch ($action) {
+        'CreateAccount' {
+            $splatCreateParams = @{
+                Uri    = $actionContext.Configuration.BaseUrl
+                Method = 'POST'
+                Body   = $actionContext.Data | ConvertTo-Json
             }
-            else {
-                $outputContext.Success = $true
-                $outputContext.AuditLogs.Add([PSCustomObject]@{
-                        Action  = "CreateAccount" # Optionally specify a different action for this audit log
-                        Message = "Dryrun Mode -- System will export succesfully with username $($account.UserName)"
-                        IsError = $false
-                    })
-            }     
-        } 
-        Catch {
-            $outputContext.Success = $false
-            $outputContext.AuditLogs.Add([PSCustomObject]@{
-                    Action  = "CreateAccount" # Optionally specify a different action for this audit log
-                    Message = "Export failed for user with username $($account.UserName)"
-                    IsError = $True
-                })
+
+            # Make sure to test with special characters and if needed; add utf8 encoding.
+            if (-not($actionContext.DryRun -eq $true)) {
+                Write-Information 'Creating and correlating TrainingIntranet account'
+                # < Write Create logic here >
+
+                $createdAccount = Invoke-RestMethod @splatCreateParams
+                $outputContext.Data = $createdAccount
+                $outputContext.AccountReference = $createdAccount.Id
+            } else {
+                Write-Information '[DryRun] Create and correlate TrainingIntranet account, will be executed during enforcement'
+            }
+            $auditLogMessage = "Create account was successful. AccountReference is: [$($outputContext.AccountReference)]"
+            break
+        }
+
+        'CorrelateAccount' {
+            Write-Information 'Correlating TrainingIntranet account'
+
+            $outputContext.Data = $correlatedAccount
+            $outputContext.AccountReference = $correlatedAccount.Id
+            $outputContext.AccountCorrelated = $true
+            $auditLogMessage = "Correlated account: [$($outputContext.AccountReference)] on field: [$($correlationField)] with value: [$($correlationValue)]"
+            break
         }
     }
 
-    $outputContext.Data = $account
+    $outputContext.success = $true
+    $outputContext.AuditLogs.Add([PSCustomObject]@{
+            Action  = $action
+            Message = $auditLogMessage
+            IsError = $false
+        })
+} catch {
+    $outputContext.success = $false
+    $ex = $PSItem
+    if ($($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or
+        $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
+        $errorObj = Resolve-TrainingIntranetError -ErrorObject $ex
+        $auditMessage = "Could not create or correlate TrainingIntranet account. Error: $($errorObj.FriendlyMessage)"
+        Write-Warning "Error at Line '$($errorObj.ScriptLineNumber)': $($errorObj.Line). Error: $($errorObj.ErrorDetails)"
+    } else {
+        $auditMessage = "Could not create or correlate TrainingIntranet account. Error: $($ex.Exception.Message)"
+        Write-Warning "Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($ex.Exception.Message)"
+    }
+    $outputContext.AuditLogs.Add([PSCustomObject]@{
+            Message = $auditMessage
+            IsError = $true
+        })
 }
