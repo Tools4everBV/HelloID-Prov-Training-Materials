c21bb344bbac55f8a79ef0ad66ba09b360bb9d74
r.jongbloed@tools4ever.com <r.jongbloed@tools4ever.com>
2025-07-14 21:07:30 +0200

    cleanup

--- application management/lab 06/formatInitials.js
+++
@@ -1,8 +0,0 @@
-function formatInitials() {
-    let initials;
-    if ((!Person.Name.NickName === "") || (Person.Name.NickName != null)) {
-        initials = ""; //please write your custom formatting logic here
-    }
-    return initials;
-}
-formatInitials();
